4b8b1b96bef18f03a23cbc215a4afe04776aa751
Kevin Eliezer <kevineli.gp@gmail.com>
2020-11-04 15:11:10 +0000

    FIX: deleted sigma example and add vis_js example with npm

--- docs/GraphLibrary_Examples/vis_js/js/app.js
+++ docs/GraphLibrary_Examples/vis_js/js/app.js
@@ -1,26 +1,33 @@
+
+import { DataSet } from "vis-data/peer";
+import { Network } from "vis-network/peer";
+import "vis-network/styles/vis-network.css";
+
 // create an array with nodes
-var nodes = new vis.DataSet([
-    { id: 1, label: "Node 1" },
-    { id: 2, label: "Node 2" },
-    { id: 3, label: "Node 3" },
-    { id: 4, label: "Node 4" },
-    { id: 5, label: "Node 5" },
+const nodes = new DataSet([
+  { id: 1, label: "Node 1" },
+  { id: 2, label: "Node 2" },
+  { id: 3, label: "Node 3" },
+  { id: 4, label: "Node 4" },
+  { id: 5, label: "Node 5" }
 ]);
 
 // create an array with edges
-var edges = new vis.DataSet([
-    { from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 },
-    { from: 3, to: 3 },
+const edges = new DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+  { from: 2, to: 5 },
+  { from: 3, to: 3 }
 ]);
 
 // create a network
-var container = document.getElementById("myDiagramDiv");
-var data = {
-    nodes: nodes,
-    edges: edges,
+const container = document.getElementById("myDiagramDiv");
+const data = {
+  nodes: nodes,
+  edges: edges
 };
-var options = {};
-var network = new vis.Network(container, data, options);
+
+const options = {};
+const network = new Network(container, data, options);
+      
