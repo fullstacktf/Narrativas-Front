958b15b6eb8f122e265e5e070b7d4158c6a20abc
Kevin Eliezer <kevineli.gp@gmail.com>
2020-12-02 13:08:43 +0000

    Add Slider Component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,18 @@
 <template>
-  <div id="app" class="flex mx-8 space-x-8">
-    <PersonCard name="Kevin Eliezer García Peña" filename="Kevin" description="Sexy y guapeton aguacate de chocolate con tarta de queso lorem lipsum y croquetas de avellana" />
-    <PersonCard name="Himar Manuel Barquín Carrasco" filename="Himar2" description="Sexy y guapeton" />
-    <PersonCard name="Alejandro Ravelo Moreno" filename="Alex" description="Sexy y guapeton" />
-    <PersonCard name="Adal Díaz Fariña" filename="Adal2" description="Sexy y guapeton" />
+  <div id="app">
+    <Slider/>
   </div>
 </template>
 
 <script>
-import PersonCard from "./components/PersonCard.vue";
+import Slider from "./components/Slider.vue";
 
 export default {
   name: "App",
   components: {
-    PersonCard
+    Slider
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-img {
-  margin: auto;
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ module.exports = {
     './src/**/*.vue'
   ],
   theme: {
-    extend: {}
+    extend: {
+      spacing: {
+        '52': '13rem',
+        '80': '20rem',
+        '88': '22rem',
+        '96': '24rem',
+        '104': '26rem',
+        '112': '28rem',
+        '120': '30rem',
+        '128': '32rem',
+      }
+    },
+    backgroundImage: ({
+      'slide-1': "url('../img/slide_1.jpg')",
+      'slide-2': "url('../img/slide_2.jpg')",
+    }),
   },
   variants: {},
   plugins: []
