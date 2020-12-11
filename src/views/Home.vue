eeea8f9559c1d5e6186fc7abbeee8d4cb96addf1
Alex Ravelo <alexravelo.dev@gmail.com>
2020-12-11 17:38:02 +0000

    Add StoryCharacter component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <Hero/>
-    <InformationBlock/>
-    <LateralSlide image="parallaxInicio"/>
-    <StoryGrid title="Explore some stories"/>
+    <Hero />
+    <InformationBlock />
+    <LateralSlide image="parallaxInicio" />
+    <StoryGrid title="Explore some stories" />
+    <StoryCharacterCard
+      name="Buzz"
+      title="Description"
+      content="Lorem ipsum bla bla bla"
+      filename="character_1"
+    />
   </div>
 </template>
 
@@ -12,15 +18,16 @@ import Hero from "@/components/Hero/Hero.vue";
 import InformationBlock from "@/components/InformationBlock/InformationBlock.vue";
 import LateralSlide from "@/components/Slider/LateralSlide.vue";
 import StoryGrid from "@/components/ImagesGrid/StoryGrid.vue";
+import StoryCharacterCard from "@/components/StoryCharacterCard/StoryCharacterCard.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Hero,
     InformationBlock,
     LateralSlide,
     StoryGrid,
-  }
-}
+    StoryCharacterCard,
+  },
+};
 </script>
-
