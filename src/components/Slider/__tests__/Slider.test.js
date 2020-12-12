64d7147db23b8916aa144f299f837a9614b027c8
xavyan <xavieryanquin@gmail.com>
2020-12-12 13:30:50 +0000

    Created test for SignInSignUp component

--- src/components/Slider/__tests__/Slider.test.js
+++ src/components/Slider/__tests__/Slider.test.js
@@ -4,9 +4,34 @@ import { fireEvent } from '@testing-library/dom';
 import { render, screen } from '@testing-library/vue';
 
 describe('Slider', () => {
-  test('should render component', () => {
-    render(Slider);
-    const slider = screen.queryByText('CREATE YOUR');
-    expect(slider).toBeInTheDocument();
+  describe('when rendering the component', () => {
+    test('should render component', () => {
+      render(Slider);
+  
+      const slider = screen.queryByRole('slider');
+      expect(slider).toBeInTheDocument();
+    });
+  });
+
+  describe('when rendering character slide', () => {  
+    test('should render title', () => {
+      render(Slider);
+  
+      const title1 = screen.queryByText('CREATE YOUR');
+      const title2 = screen.queryByText('CHARACTERS');
+      expect(title1).toBeInTheDocument();
+      expect(title2).toBeInTheDocument();
+    });
+  
+    test('should render button', () => {
+      render(Slider);
+  
+      const button = screen.queryByText('Get Started');
+      expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe('when rendering stories slide', () => {
+    // TODO
   });
 });
