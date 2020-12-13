e41858122d19af340a5d626cda1ff98b44bba291
Alex Ravelo <alexravelo.dev@gmail.com>
2020-12-13 17:42:08 +0000

    Fix last commit

--- src/views/CharacterCreation.vue
+++ src/views/CharacterCreation.vue
@@ -1,21 +1,12 @@
 <template>
-    <Character />
+  <Character />
 </template>
 
 <script>
-import Character from '../components/Characters/character.vue'
+import Character from "../components/Characters/character.vue";
 export default {
-<<<<<<< HEAD
-  components: { 
-    Character 
-  },
-
-=======
-  components: { character },
->>>>>>> 570b391f842072f2f138bdffe2c10f92219e26b1
-}
+  components: { Character },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
